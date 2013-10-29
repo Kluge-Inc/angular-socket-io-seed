a418a2c1e6205b0d7217628617886700be20e7ff
theycallmeS <224.0.0.25@gmail.com>
2013-10-29 11:46:58 +0400

    re-sending messages from socket to rabbit

--- routes/socket.js
+++ routes/socket.js
@@ -11,6 +11,7 @@ module.exports = function (socket) {
 
   socket.on('message', function (data) {
     console.log(" [s] Message received from socket: %s", data);
+    exchangeGlobal.publish('', data);
   });
 
 };
